34b2b42e45b1eaa5b7979a8cd019733d02c2f273
jougy <46467326+jougy@users.noreply.github.com>
2019-09-12 14:53:34 -0300

    jougy
    led

--- valet.py
+++ valet.py
@@ -2,29 +2,33 @@ import RPi.GPIO as GPIO
 from time import sleep
 import sys
 import os
-
+led01 = 18
 MotorA_PIN = 12
 MotorB_PIN = 13
 en = 25
 bt1 = 15
 temp1=1
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led01,GPIO.OUT)
 GPIO.setup(MotorA_PIN,GPIO.OUT)
 GPIO.setup(MotorB_PIN,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
 GPIO.setup(bt1, GPIO.IN)
 GPIO.output(MotorA_PIN,GPIO.LOW)
 GPIO.output(MotorB_PIN,GPIO.LOW)
+GPIO.output(led01,GPIO.LOW)
 p=GPIO.PWM(en,100)
 
 p.start(25)
 
 def frente():
+	GPIO.output(led01,GPIO.HIGH)
 	GPIO.output(in1,GPIO.HIGH)
 	GPIO.output(in2,GPIO.LOW)
 	print("frente")
 def tras():
+	GPIO.output(led01,GPIO.LOW)
 	GPIO.output(in1,GPIO.LOW)
 	GPIO.output(in2,GPIO.HIGH)
 	print("tras")
