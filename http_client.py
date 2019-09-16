7ff391bc436e21faa0312f419dbcd5b5a419c8a7
jougy <jougygames@gmail.com>
2019-09-15 21:50:11 -0300

    botao de parar e novos thread

--- http_client.py
+++ http_client.py
@@ -1,26 +1,30 @@
 from flask import Flask
 import valet
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print('\n')
-	return 'Projeto Vivo\nValet Iot'
+    return 'projeto vivo'    
 
 @app.route('/abrir')
 def frente():
     valet.frente()
     return 'cancela abrindo'
-
 @app.route('/fechar')
 def tras():
     valet.tras()
     return 'cancela fechando'
 @app.route('/parar')
 def parar():
-	valet.parar()
-	return 'parando motores'
+    valet.parar()
+    return 'parando motores'
+
+
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=80)
+    t1 = threading.Thread(target=valet.botao, args=[])
+    t1.start()
+    app.run(host='0.0.0.0', port=80)
+    
--- valet.py
+++ valet.py
@@ -1,68 +1,64 @@
-import RPi.GPIO as GPIO
-from time import sleep
+import RPi.GPIO as a
+import time
 import sys
 import os
-led01 = 18
-MotorA_PIN = 12
-MotorB_PIN = 13
-en = 25
-bt1 = 36
-bt2 = 37
-temp1=1
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led01,GPIO.OUT)
-GPIO.setup(MotorA_PIN,GPIO.OUT)
-GPIO.setup(MotorB_PIN,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.setup(bt1, GPIO.IN)
-GPIO.setup(bt2, GPIO.IN)
-GPIO.output(MotorA_PIN,GPIO.LOW)
-GPIO.output(MotorB_PIN,GPIO.LOW)
-GPIO.output(led01,GPIO.LOW)
-p=GPIO.PWM(en,100)
+mA = 12
+mB = 13
+sfc1 = 18
+sfc2 = 32
 
-p.start(25)
+a.setmode(a.BOARD)
 
-def frente():
-	GPIO.output(led01,GPIO.HIGH)
-	GPIO.output(in1,GPIO.HIGH)
-	GPIO.output(in2,GPIO.LOW)
-	print("frente")
-def tras():
-	GPIO.output(led01,GPIO.LOW)
-	GPIO.output(in1,GPIO.LOW)
-	GPIO.output(in2,GPIO.HIGH)
-	print("tras")
+a.setup (mA, a.OUT)
+a.setup (mB, a.OUT)
+a.setup (sfc1, a.IN)
+a.setup (sfc2, a.IN)
+
+
 
 def parar():
-	print("stop")
-	GPIO.output(in1,GPIO.LOW)
-	GPIO.output(in2,GPIO.LOW)
-
-if __name__ == "__main__":
-
-    ''' Caso tenha sido passado argumento,
-    seta valor do Led com base nele
-    '''
-    print("sys.argv vale {}".format(sys.argv))
-    if len(sys.argv) >= 2:
-        if sys.argv[1] == 'frente':
-            frente()
-        elif sys.argv[1] == 'tras':
-            tras()
-        elif sys.argv[1] == 'parar':
-        	parar()
-
-    else:
-        try:
-            while(True):
-                parar()
-                print('aguardando...')
-        except KeyboardInterrupt:
+    print('funcao parar acionada')
+    a.output (mA, a.LOW)
+    a.output (mB, a.LOW)
+def frente():
+    print('funcao frente acionada')
+    a.output (mA, a.HIGH)
+    a.output (mB, a.LOW)
+def tras():
+    btm2 = a.input(sfc2)
+    print('funcao tras acionada')
+    a.output (mA, a.LOW)
+    a.output (mB, a.HIGH)
+def botao():
+    while True:
+        time.sleep(0.2)
+        btm1 = a.input(sfc1)
+        if btm1 == 0:
             parar()
-            os.system('clear')
-            print("saindo do programa...")
-            sys.exit()
 
+
+os.system('clear')
+if __name__ == '__main__' :
+    parar()
+    t1 = threading.Thread(target=botao, args=[])
+    t1.start()
+    while 1:
+        resposta = input('digite a funcao que vc qer executar [frente] [tras] [parar]\nSe quiser sair digite [sair]\nSua Resposta: ')
+        btm1 = a.input(sfc1)
+        btm2 = a.input(sfc2)
+        
+        if resposta == 'frente':
+            frente()
+        elif resposta == 'tras':
+            tras()
+            time.sleep(2)
+            parar()
+        elif resposta == 'parar':
+            parar()
+        elif resposta == 'sair':
+            os.system('clear')
+            exit()
+        else:
+            print('resposta invalida... Tente novamente')
 
