221ce8c5c3e34b7a9b18be1ff88b07478c5c0f8f
felipegenef <55562247+felipegenef@users.noreply.github.com>
2019-10-14 18:52:21 -0300

    Felipe Gene

--- Catraca.py
+++ Catraca.py
@@ -1,27 +1,27 @@
 from socket import *
 CTR="DISP"
-servidor ="0.0.0.0"
-porta= 80
+servidor ="localhost"
+porta= 43210
 Conexao= socket(AF_INET,SOCK_STREAM)
 Conexao.bind((servidor,porta))
 
 Conexao.listen(3)
 #--------------------------------------------------------------------
 #REFERENTE A PINAGEM
-import RPi.GPIO as a
+import RPi.GPIO as pin
 import time
 mA = 12
 mB = 13
 sensorFimdeCurso1 = 18
 sensorFimdeCurso2 = 32
-a.setmode(a.BOARD)
-a.setup (mA, a.OUT)
-a.setup (mB, a.OUT)
-a.setup (sensorFimdeCurso1, a.IN)
-a.setup (sensorFimdeCurso2, a.IN)
+pin.setmode(pin.BOARD)
+pin.setup (mA, pin.OUT)
+pin.setup (mB, pin.OUT)
+pin.setup (sensorFimdeCurso1, pin.IN)
+pin.setup (sensorFimdeCurso2, pin.IN)
 
-m1 = a.PWM (mA, 1000)
-m2 = a.PWM (mB, 1000)
+m1 = pin.PWM (mA, 1000)
+m2 = pin.PWM (mB, 1000)
 m1.start(0)
 m2.start(0)
 #-----------------------------------------------------------
@@ -37,23 +37,23 @@ while 1:
     msgRecebida = comandoRecebido.decode('utf-8')
 
     if msgRecebida == "LEVANTARCATRACA":
-        while a.input(sensorFimdeCurso1) == 0:  # Sensor ultrasonico
+        while pin.input(sensorFimdeCurso1) == 0:  # Sensor ultrasonico
             CRT="DISP"
             Conexao.send(bytes(CTR,'utf-8')
             # CONSEGUIR DADOS FINAIS E CALCULAR
             # ENVIAR DADOS
-            a.output(mA, a.HIGH)
-            a.output(mB, a.LOW)
+            pin.output(mA, pin.HIGH)
+            pin.output(mB, pin.LOW)
             m1.ChangeDutyCycle(100)
             m2.ChangeDutyCycle(0)
             Conexao.close()
-    elif msgRecebida == "ABAIXARCATRACA":
-        while a.input(sensorFimdeCurso2) == 0:
+    elif msgRecebida == "ABAIXACATRACA":
+        while pin.input(sensorFimdeCurso2) == 0:
             CRT="INDS"
             # ARMAZENAR ESTADO DA CATRACA
             # CONSEGUIR DADOS INICIAIS
-            a.output(mA, a.LOW)
-            a.output(mB, a.HIGH)
+            pin.output(mA, pin.LOW)
+            pin.output(mB, pin.HIGH)
             m1.ChangeDutyCycle(0)
             m2.ChangeDutyCycle(5)
             Conexao.close()
